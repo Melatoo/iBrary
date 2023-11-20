7d3036772782566cf4a55ed045d593827601aa8b
Davi Siqueira <davisvsiqueira@gmail.com>
2023-11-20 20:57:07 -0300

    closes #5; criação da rota de livro

--- Implementacao/index.js
+++ Implementacao/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
-
 const app = express();
+const livro = require("./routes/livro");
+
+// configurações
 app.use(express.json()); // configura json como content-type esperado em requisições
 
-app.listen(3000);
+// rotas
+app.use("/livro", livro);
+
+app.listen(3000, () => console.log("servidor iniciado"));
