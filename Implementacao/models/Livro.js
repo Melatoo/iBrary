c9eabaa1bbd0d966db8c7d11e043c4d48a06e0f6
Davi Siqueira <davisvsiqueira@gmail.com>
2023-11-20 20:47:31 -0300

    closes #4; criação do modelo Livro

---
+++ Implementacao/models/Livro.js
@@ -0,0 +1,30 @@
+const { sequelize, Sequelize } = require("./db");
+
+const Livro = sequelize.define("livro", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  editora: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  autor: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  dataCadastro: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+});
+
+// Livro.sync({ force: true });
+
+module.exports = Livro;
