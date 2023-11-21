55ed0e743ada5e856ed7ab90d1998393a679de9e
Davi Siqueira <davisvsiqueira@gmail.com>
2023-11-20 21:37:21 -0300

    refatoração do código

--- Implementacao/models/Livro.js
+++ Implementacao/models/Livro.js
@@ -1,28 +1,28 @@
 const { sequelize, Sequelize } = require("./db");
 
 const Livro = sequelize.define("livro", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-  },
-  nome: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  editora: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  autor: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  dataCadastro: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    editora: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    autor: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    dataCadastro: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
 });
 
 // Livro.sync({ force: true });
