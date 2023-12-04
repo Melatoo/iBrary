af1f561fef52a1ebc16ae159575bf08e4a218d4e
Davi Siqueira <davisvsiqueira@gmail.com>
2023-12-04 15:10:33 -0300

    adicionando cabeçalho dos livros

---
+++ Implementacao/front-end/src/components/Painel/CabecalhoLivros.jsx
@@ -0,0 +1,15 @@
+import "./CabecalhoLivros.css";
+
+const CabecalhoLivros = () => {
+  return (
+    <div id="cabecalhoLivros">
+      <text id="textoCabecalho">Nome</text>
+      <text id="textoCabecalho">Editora</text>
+      <text id="textoCabecalho">Autor</text>
+      <text id="textoCabecalho">ID</text>
+      <text id="textoCabecalho">Data de cadastro</text>
+    </div>
+  );
+};
+
+export default CabecalhoLivros;
--- Implementacao/front-end/src/views/Admin/Livros.jsx
+++ Implementacao/front-end/src/views/Admin/Livros.jsx
@@ -1,12 +1,12 @@
 import BackgroundPainel from "../../components/Painel/BackgroundPainel";
+import CabecalhoLivros from "../../components/Painel/CabecalhoLivros";
 import "./Livros.css";
 
 const Livros = () => {
   return (
-    <BackgroundPainel
-      tipoLista="livros"
-      textoBotao="Adicionar novo livro"
-    ></BackgroundPainel>
+    <BackgroundPainel tipoLista="livros" textoBotao="Adicionar novo livro">
+      <CabecalhoLivros />
+    </BackgroundPainel>
   );
 };
 
