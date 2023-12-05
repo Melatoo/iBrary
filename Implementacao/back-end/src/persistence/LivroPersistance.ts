93f2407b80fe4c2e5d00f0f55834f1390715129f
Davi Siqueira <davisvsiqueira@gmail.com>
2023-12-05 15:05:44 -0300

    crud completo do livro

--- Implementacao/back-end/src/persistence/LivroPersistance.ts
+++ Implementacao/back-end/src/persistence/LivroPersistance.ts
@@ -4,4 +4,16 @@ const getLivros = async () => {
   return await prisma.livro.findMany();
 };
 
-export default { getLivros };
+const createLivro = async (livro: LivroType) => {
+  return await prisma.livro.create({ data: livro });
+};
+
+const updateLivro = async (id: number, livro: LivroType) => {
+  return await prisma.livro.update({ where: { id }, data: livro });
+};
+
+const deleteLivro = async (id: number) => {
+  return await prisma.livro.delete({ where: { id } });
+};
+
+export default { getLivros, createLivro, updateLivro, deleteLivro };
