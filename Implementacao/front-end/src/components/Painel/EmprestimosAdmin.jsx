e3917c00131ed4dc69086c104211fe496de6a3b5
Davi Siqueira <davisvsiqueira@gmail.com>
2023-12-06 00:30:12 -0300

    começando listagem de empréstimos

--- Implementacao/front-end/src/components/Painel/EmprestimosAdmin.jsx
+++ Implementacao/front-end/src/components/Painel/EmprestimosAdmin.jsx
@@ -1,4 +1,6 @@
 import CardEmprestimo from "./Cards/Emprestimo";
+import axios from "../../services/axios";
+import { useEffect, useState } from "react";
 
 const CabecalhoEmprestimos = () => {
   return (
@@ -16,11 +18,30 @@ const CabecalhoEmprestimos = () => {
 };
 
 const PainelEmprestimosAdmin = () => {
+  const [emprestimos, setEmprestimos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/emprestimos")
+      .then((res) => {
+        setEmprestimos(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div id="tabela">
       <table>
         <CabecalhoEmprestimos />
         <tbody>
+          {emprestimos.map((emprestimo) => (
+            <CardEmprestimo
+              idLivro={emprestimo.idLivro}
+              matricula={emprestimo.matricula}
+              duracao={emprestimo.duracao}
+            />
+          ))}
           <CardEmprestimo
             idLivro="0"
             matricula="202210077"
